fe41d11136a8e84e1afb83862053df7a651c4b3c
Daniel Hentosz <57598807+danhentosz@users.noreply.github.com>
2021-02-15 03:39:21 -0500

    Add files via upload
    Submit this, if all looks good to you.

--- Program2.java
+++ Program2.java
@@ -6,252 +6,270 @@
  * hen3883@calu.edu || deh5850@calu.edu
  * GROUP 7
  */
-import java.io.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.StringTokenizer;
+import java.io.IOException;
 
 class Program2 {
 	public static void main(String[] args) {
-		int i = 0;
+		IOFile o1 = new IOFile();
+		BufferedReader get;
+		StringTokenizer tk;
+		String ionames[] = new String[3];
+		String temp, temp2, temp3;
 		int count = 0;
 		int sum = 0;
-		String word = null;
-		String[] ionames = new String[3];
+		int i;
+		PrintWriter say;
 		Word words[] = new Word[100];
-		StringTokenizer inline;
-		BufferedReader infile;
-		PrintWriter outfile;
-		File in;
-		IOFile f1 = new IOFile();
-		String temp = new String("");
-		if (f1.getnames(args, ionames)) {
-			// They entered a valid input and output file path
-			in = new File(ionames[0]);
-			ionames[2] = null;
-			if (f1.choice(ionames)) {
-				// They selected 1 or 2(backup the file or overwrite the file)
-				try {
-					infile = new BufferedReader(new FileReader(in));
-					while ((word = infile.readLine()) != null) {
-						// read every line
-						inline = new StringTokenizer(word, "~`!@#$%^&*()_+=[{]}\\|\n\t\r,<>./?;:\" ");
-						// each line beacomes several tokens
-						while (inline.hasMoreTokens()) {
-							word = inline.nextToken().toString().toLowerCase();
-							// each token is made a string, so we can use our String methods
-							temp = "0";
-							if (f1.good(word)) {
-								// Checks if the string is just - or '("----" or "''''''")
-								if (word.length() > 1) {
-									// So we dont get a index out of bounds exception when trying see see slot 2
-									while (word.charAt(1) == '-') {
-										word = word.substring(1);
-									}
+		// If they enter a valid input and output file;
+		if (o1.get(args, ionames)) {
+			try {
+				get = new BufferedReader(new FileReader(ionames[0]));
+				// Keep reading each line until the end of the input file.
+				while ((temp = get.readLine()) != null) {
+					// Creating a StringTokenizer object for each line
+					tk = new StringTokenizer(temp, "`~!@#$%^&*()_=+[]{};:\",<.>/?\n\t\r ");
+					// Read every token and assign a string to the token each time.
+					while (tk.hasMoreTokens()) {
+						temp3 = "0";
+						temp2 = tk.nextToken().toLowerCase().toLowerCase();
+						// Check rather the string has any valid characters("-----" and "'''''").
+						if (o1.good(temp2)) {
+							;// Making sure we can get the second character index.
+							if (temp2.length() > 1) {
+								// Getting rid of excessive leading '-' and "'"
+								while (temp2.charAt(1) == '-' | temp2.charAt(0) == '\'') {
+									temp2 = temp2.substring(1);
 								}
-								try {
-									sum = sum + Integer.parseInt(word);
-								} catch (NumberFormatException e) {
-									// We know its either a word or numbers followed by a word(123cat)
-									char c = word.charAt(0);
-									while (word.charAt(0) == '-' | word.charAt(0) == '\'' | Character.isDigit(c)) {
-										if (Character.isDigit(c)) {
-											// If its number followed by a word(123cat)
-											temp = temp + Character.toString(c);
-										}
-										word = word.substring(1);
-										c = word.charAt(0);
+							}
+							try {
+								// If its not a number, we will take care of it in our catch block.
+								sum = sum + Integer.parseInt(temp2);
+							} catch (NumberFormatException e) {
+								// Now its either a word, or a number followed by a word(-123cat or 123cat).
+								char c = temp2.charAt(0);
+								// Seeing if its a negitive number followed by a word(-123cat).
+								if (c == '-') {
+									temp3 = "-0";
+								}
+								// Removing all excessive leading signs(' and -)
+								while (temp2.charAt(0) == '-' | temp2.charAt(0) == '\'' | Character.isDigit(c)) {
+									// If there are numbers before the word.
+									if (Character.isDigit(c)) {
+										temp3 = temp3 + Character.toString(c);
 									}
-									sum = sum + Integer.parseInt(temp);
-									// Creating our new Word object(if its needed)
-									if (count == 0) {
-										words[0] = new Word(word);
+									temp2 = temp2.substring(1);
+									c = temp2.charAt(0);
+								}
+								// Adding the potential number before the word.
+								sum = sum + Integer.parseInt(temp3);
+								// If its the first element in are obhect array of Word.
+								if (count == 0) {
+									words[0] = new Word(temp2);
+									count++;
+								} else {
+									// Its not the first element in the array.
+									// i will be negative 1 if the word does not exist, otherwise, it will be the
+									// index of which the word exists.
+									i = words[0].find(temp2, count, words);
+									if (i == -1) {
+										words[count] = new Word(temp2);
 										count++;
 									} else {
-										// i will be -1 if the word is not already in the array
-										i = words[0].findWord(words, word, count);
-										if (i == -1) {
-											words[count] = new Word(word);
-											count++;
-										} else {
-											words[i].addOne();
-											;
-										}
+										words[i].add();
 									}
 								}
 							}
 						}
 					}
-				} catch (IOException e) {
-					e.printStackTrace();
 				}
-				try {
-					// Writing to the file
-					// if ionames[2] is null then the user wants to overwrite the exiting output
-					// file
-					if (ionames[2] != null)
-						outfile = new PrintWriter(new File(ionames[2]));
-					else
-						outfile = new PrintWriter(new File(ionames[1]));
-					for (int j = 0; j < count; j++) {
-						outfile.write("WORD:    " + words[j].getName());
-						outfile.write("\nCOUNT:    ");
-						outfile.print(words[j].getCount());
-						outfile.write("\n");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			// If they entered 1 or 2 for their choice of overwriting the file or not.
+			if (o1.choice(ionames)) {
+				// Index 2 will be null if they decided to simply overwrite the output file.
+				if (ionames[2] == null) {
+					try {
+						say = new PrintWriter(new File(ionames[1]));
+						o1.print(say, count, words, sum);
+						say.close();
+					} catch (FileNotFoundException e) {
+						e.printStackTrace();
 					}
-					outfile.write("TOTAL UNIQUE WORDS:    ");
-					outfile.print(count);
-					outfile.write("\nSUM OF INTEGERS:    ");
-					outfile.print(sum);
-					outfile.close();
-				} catch (FileNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+				} else {
+					try {
+						say = new PrintWriter(new File(ionames[2]));
+						o1.print(say, count, words, sum);
+						say.close();
+					} catch (FileNotFoundException e) {
+						e.printStackTrace();
+					}
 				}
 			}
 		}
 	}
 }
 
-class Word {
-	private String name;
-	private int count;
-
-	// Contructor
-	Word(String w) {
-		name = w;
-		count = 1;
-	}
-
-	// To display the count
-	int getCount() {
-		return count;
-	}
-
-	// To increment the count
-	void addOne() {
-		count++;
-	}
-
-	// To display the unique word
-	String getName() {
-		return name;
-	}
-
-	// To see if the word is already in the array
-	// Returns -1 if not, otherwise returns the index of which the array exists
-	int findWord(Word[] list, String w, int n) {
-		for (int i = 0; i < n; i++) {
-			if (list[i].name.equals(w)) {
-				return i;
-			}
-		}
-		return -1;
-	}
-}
-
+//Contains operations for everything related to the files.
 class IOFile {
-	// Returns true if both input and output file names are present
-	boolean getnames(String args[], String[] ionames) {
+	// Gets valid file paths and returns them to the ionames array.
+	boolean get(String args[], String ionames[]) {
 		BufferedReader get = new BufferedReader(new InputStreamReader(System.in));
-		File in = new File("");
-		File out = new File("");
-		String temp = new String(" ");
-		// Seeing how many command line arguments they entered, if any at all
+		File f1 = new File("");
+		File f2 = new File("");
+		// Checking the command line argument length.
 		switch (args.length) {
 		case 1:
-			in = new File(args[0]);
 			ionames[0] = args[0];
+			f1 = new File(ionames[0]);
 			break;
 		case 2:
-			in = new File(args[0]);
-			out = new File(args[1]);
 			ionames[0] = args[0];
 			ionames[1] = args[1];
+			f1 = new File(ionames[0]);
+			f2 = new File(ionames[1]);
 			break;
 		default:
 			break;
 		}
-		// While the input file path is incorrect or the user didnt enter anything
-		while (!in.exists() && !temp.isEmpty()) {
-			System.out.println("Enter a valid input file path:");
+		String choice = new String(" ");
+		if (f1.exists() && f2.exists())
+			return true;
+		// Keep looping until they enter nothing or a valid file path.
+		while (!f1.exists() && !choice.isEmpty()) {
+			System.out.println("Enter a valid input file name:");
 			try {
-				temp = get.readLine();
-				// update our array
-				ionames[0] = temp;
-				in = new File(temp);
+				choice = get.readLine();
+				// Updating our ionames array.
+				ionames[0] = choice;
+				f1 = new File(choice);
 			} catch (IOException e) {
-				e.printStackTrace();
+
 			}
 		}
-		// While the output file path is incorrect or the user didnt enter anything
-		while (!out.exists() && !temp.isEmpty()) {
+		// Keep looping until they enter nothing or a valid file path.
+		while (!f2.exists() && !choice.isEmpty()) {
 			System.out.println("Enter a valid output file path:");
 			try {
-				temp = get.readLine();
-				// update our array
-				ionames[1] = temp;
-				out = new File(temp);
+				choice = get.readLine();
+				// Updating our ionames array.
+				ionames[1] = choice;
+				f2 = new File(choice);
 			} catch (IOException e) {
-				e.printStackTrace();
+
 			}
 		}
-		if (in.exists() && out.exists()) {
+		if (f1.exists() && f2.exists()) {
+			ionames[0].replaceAll(" ", "");
+			ionames[1].replaceAll(" ", "");
 			return true;
 		}
 		return false;
 	}
 
-	// Returns true if they enter 1 and a valid new output file path
-	// Returns true if they enter 2(To overwrite the output file
-	// Returns false otherwise
-	boolean choice(String ionames[]) {
-		File f = new File("");
-		String choice = " ";
-		String choice2 = " ";
-		BufferedReader get = new BufferedReader(new InputStreamReader(System.in));
-		while (!choice.isEmpty()) {
-			// prompt the user
-			System.out.println("PRESS 1: To backup the exitsing output file");
-			System.out.println("PRESS 2: To overwrite the exitsing output file");
-			System.out.println("PRESS 3: To exit");
-			try {
-				choice = get.readLine();
-				if (choice.equals("1")) {
-					while (!choice2.isEmpty() && !f.exists()) {
-						System.out.println("Enter a new valid output file name:");
-						choice2 = get.readLine();
-						if (choice2.isEmpty())
-							return false;
-						f = new File(choice2);
-					}
-					if (f.exists()) {
-						// putting data here, we will know their choice later in the program by
-						// comparing ionames[2] to null
-						ionames[2] = choice2;
-						return true;
-					}
-				} else if (choice.equals("2")) {
-					return true;
-				} else if (choice.equals("3")) {
-					return false;
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return false;
-	}
-
-	// Check if the string is just - or '
-	// We need this because we would get an ArrayIndexOutfBoundsException if we
-	// didnt have it
+	// Checks if a string ha any valid characters("----" and "''''''").
 	boolean good(String s) {
 		char c;
 		for (int i = 0; i < s.length(); i++) {
 			c = s.charAt(i);
-			if (Character.isLetter(c) | Character.isDigit(c)) {
+			if (Character.isDigit(c) | Character.isLetter(c))
 				return true;
-			}
 		}
 		return false;
+	}
+
+	// Returns true if they enter 1 or 2, and a file path in ionames[2] if they wish
+	// to make a backup file.
+	boolean choice(String ionames[]) {
+		String temp = " ";
+		BufferedReader get;
+		String choice = " ";
+		String name1;
+		while (!choice.isEmpty()) {
+			System.out.println(
+					"Enter 1 to overwrite the output file.\nEnter 2 to back up the original output file.\nEnter 3 to exit.");
+			get = new BufferedReader(new InputStreamReader(System.in));
+			try {
+				choice = get.readLine();
+			} catch (IOException e) {
+
+			}
+			if (choice.equals("1"))
+				return true;
+			else if (choice.equals("2")) {
+				// Name1 will be the new file name for the backup file.
+				name1 = sep(ionames[1]);
+				ionames[2] = name1;
+				return true;
+			} else if (choice.equals("3"))
+				return false;
+		}
+		return false;
+	}
+
+	// Used to get the file name, and add "BACKUP" to the name.
+	String sep(String s) {
+		int i = s.lastIndexOf('\\');
+		i++;
+		int j = s.lastIndexOf('.');
+		String nn = s.substring(0, i);
+		nn = nn + s.substring(i, j);
+		nn = nn + "BACKUP";
+		nn = nn + s.substring(j);
+		return nn;
+	}
+
+	void print(PrintWriter out, int n, Word words[], int sum) {
+		for (int i = 0; i < n; i++) {
+			out.print("Word: " + words[i].getname());
+			out.print("\nCount: " + words[i].getcount() + "\n");
+		}
+		out.print("Total unique words: " + n);
+		out.print("\nSum: " + sum);
+	}
+}
+
+//Our word class to make object which will contain the word and count.
+class Word {
+	private String name;
+	private int count;
+
+	// Constructor
+	Word(String s) {
+		name = s;
+		count = 1;
+	}
+
+	// Increment count by one.
+	void add() {
+		count++;
+	}
+
+	// get the count.
+	int getcount() {
+		return count;
+	}
+
+	// Get the name.
+	String getname() {
+		return name;
+	}
+
+	// Returns -1 if the string is not in the object array of type Word, otherwise
+	// it returns the index of which the string exists.
+	int find(String s, int n, Word w[]) {
+		for (int i = 0; i < n; i++) {
+			if (w[i].getname().equals(s))
+				return i;
+		}
+		return -1;
 	}
 }
