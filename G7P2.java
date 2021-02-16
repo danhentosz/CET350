e80a30008db62cea1186865b1e87e679a234bab3
deh5850 <78886396+deh5850@users.noreply.github.com>
2021-02-16 11:10:13 -0500

    Add files via upload

--- G7P2.java
+++ G7P2.java
@@ -11,67 +11,71 @@ import java.util.StringTokenizer;
 
 class G7P2 {
 	public static void main(String[] args) {
-		int i = 0;
+		IOFile o1 = new IOFile();
+		BufferedReader get;
+		StringTokenizer tk;
+		String ionames[] = new String[3];
+		String temp, temp2, temp3;
 		int count = 0;
 		int sum = 0;
-		String word = null;
-		String[] ioFilePaths = new String[2];
+		int i;
+		PrintWriter say;
 		Word words[] = new Word[100];
-		StringTokenizer inline;
-		BufferedReader infile;
-		PrintWriter outfile;
-		File in;
-		IOFile f1 = new IOFile();
-		String temp = new String("");
-		if (f1.getnames(args, ioFilePaths)) {
-			// They entered a valid input and output file path
-			in = new File(ioFilePaths[0]);
-			
+		if (o1.getnames(args, ionames)) {
 			try {
-				infile = new BufferedReader(new FileReader(in));
-				while ((word = infile.readLine()) != null) {
-					// read every line
-					inline = new StringTokenizer(word, "~`!@#$%^&*()_+=[{]}\\|\n\t\r,<>./?;:\" ");
-					// each line becomes several tokens
-					while (inline.hasMoreTokens()) {
-						word = inline.nextToken().toString().toLowerCase();
-						// each token is made a string, so we can use our String methods
-						temp = "0";
-						if (f1.good(word)) {
-							// Checks if the string is just - or '("----" or "''''''")
-							if (word.length() > 1) {
-								// So we don't get an index out of bounds exception when trying see see slot 2
-								while (word.charAt(1) == '-') {
-									word = word.substring(1);
+				get = new BufferedReader(new FileReader(ionames[0]));
+				// Keep reading each line until the end of the input file.
+				while ((temp = get.readLine()) != null) {
+					// Creating a StringTokenizer object for each line
+					tk = new StringTokenizer(temp, "`~!@#$%^&*()_=+[]{};:\",<.>/?\n\t\r ");
+					// Read every token and assign a string to the token each time.
+					while (tk.hasMoreTokens()) {
+						temp3 = "0";
+						temp2 = tk.nextToken().toLowerCase().toLowerCase();
+						// Check rather the string has any valid characters("-----" and "'''''").
+						if (o1.good(temp2)) {
+							;// Making sure we can get the second character index.
+							if (temp2.length() > 1) {
+								// Getting rid of excessive leading '-' and "'"
+								while (temp2.charAt(1) == '-' | temp2.charAt(0) == '\'') {
+									temp2 = temp2.substring(1);
 								}
 							}
 							try {
-								sum = sum + Integer.parseInt(word);
+								// If its not a number, we will take care of it in our catch block.
+								sum = sum + Integer.parseInt(temp2);
 							} catch (NumberFormatException e) {
-								// We know its either a word or numbers followed by a word(123cat)
-								char c = word.charAt(0);
-								while (word.charAt(0) == '-' | word.charAt(0) == '\'' | Character.isDigit(c)) {
-									if (Character.isDigit(c)) {
-										// If its number followed by a word(123cat)
-										temp = temp + Character.toString(c);
-									}
-									word = word.substring(1);
-									c = word.charAt(0);
+								// Now its either a word, or a number followed by a word(-123cat or 123cat).
+								char c = temp2.charAt(0);
+								// Seeing if its a negitive number followed by a word(-123cat).
+								if (c == '-') {
+									temp3 = "-0";
 								}
-								sum = sum + Integer.parseInt(temp);
-								// Creating our new Word object(if its needed)
+								// Removing all excessive leading signs(' and -)
+								while (temp2.charAt(0) == '-' | temp2.charAt(0) == '\'' | Character.isDigit(c)) {
+									// If there are numbers before the word.
+									if (Character.isDigit(c)) {
+										temp3 = temp3 + Character.toString(c);
+									}
+									temp2 = temp2.substring(1);
+									c = temp2.charAt(0);
+								}
+								// Adding the potential number before the word.
+								sum = sum + Integer.parseInt(temp3);
+								// If its the first element in are obhect array of Word.
 								if (count == 0) {
-									words[0] = new Word(word);
+									words[0] = new Word(temp2);
 									count++;
 								} else {
-									// i will be -1 if the word is not already in the array
-									i = words[0].findWord(words, word, count);
+									// Its not the first element in the array.
+									// i will be negative 1 if the word does not exist, otherwise, it will be the
+									// index of which the word exists.
+									i = Word.findWord(words, temp2, count);
 									if (i == -1) {
-										words[count] = new Word(word);
+										words[count] = new Word(temp2);
 										count++;
 									} else {
 										words[i].addOne();
-										;
 									}
 								}
 							}
@@ -82,22 +86,11 @@ class G7P2 {
 				e.printStackTrace();
 			}
 			try {
-				// Writing to the file
-				outfile = new PrintWriter(new File(ioFilePaths[1]));
-				for (int j = 0; j < count; j++) {
-					outfile.write("WORD:    " + words[j].getName());
-					outfile.write("\nCOUNT:    ");
-					outfile.print(words[j].getCount());
-					outfile.write("\n");
-				}
-				outfile.write("TOTAL UNIQUE WORDS:    ");
-				outfile.print(count);
-				outfile.write("\nSUM OF INTEGERS:    ");
-				outfile.print(sum);
-				outfile.close();
-				System.out.println("\nSuccessfully wrote data to: " + ioFilePaths[1]);
+				say = new PrintWriter(new File(ionames[1]));
+				o1.print(say, count, words, sum);
+				say.close();
+				System.out.println("\nSuccessfully wrote data to: " + ionames[1]);
 			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -131,7 +124,7 @@ class Word {
 
 	// To see if the word is already in the array
 	// Returns -1 if not, otherwise returns the index of which the array exists
-	int findWord(Word[] list, String w, int n) {
+	static int findWord(Word[] list, String w, int n) {
 		for (int i = 0; i < n; i++) {
 			if (list[i].name.equals(w)) {
 				return i;
@@ -146,8 +139,9 @@ class IOFile {
 	boolean getnames(String args[], String[] ioFilePaths) {
 		BufferedReader get = new BufferedReader(new InputStreamReader(System.in));
 		File in = new File("");
-		File out = new File("");
+		File out = null;
 		boolean quitting = false;
+		boolean invalidOut = true;
 		String temp = new String(" ");
 		// Seeing how many command line arguments they entered, if any at all
 		switch (args.length) {
@@ -166,22 +160,19 @@ class IOFile {
 		}
 		// While the input file path is incorrect
 		while (!in.exists() && !quitting) {
-			System.out.println("\nEnter a valid input file path:");
+			System.out.print("\nEnter a valid input file path:\n> ");
 			try {
-				System.out.print("> ");
 				temp = get.readLine();
 				// update our array
 				ioFilePaths[0] = new String(temp);
 				in = new File(temp);
-				if (!in.exists())
-				{
+				if (!in.exists()) {
 					System.out.println("\nThe entered file path does not exist."
 							+ "\nEnter 1 to try again"
 							+ "\nEnter anything else to exit");
 					System.out.print("> ");
 					temp = get.readLine();
-					if (!temp.equals("1"))
-					{
+					if (!temp.equals("1")) {
 						quitting = true;
 					}
 				}
@@ -189,51 +180,63 @@ class IOFile {
 				e.printStackTrace();
 			}
 		}
-		// While the output file path is incorrect
-		while (!out.exists() && !quitting) {
-			System.out.println("\nEnter an output file path:");
+		// While the output file path is invalid
+		while (invalidOut && !quitting) {
 			try {
-				System.out.print("> ");
-				temp = get.readLine();
-				// update our array
-				ioFilePaths[1] = new String(temp);
-				out = new File(temp);
-				// Checks if a file with the output file name already exists
-				if (!out.createNewFile())
-				{
-					System.out.println("\nAn output file with that name already exists."
-							+ "\nEnter 1 to overwrite it"
-							+ "\nEnter 2 to back up the original"
-							+ "\nEnter anything else to exit");
-					System.out.print("> ");
+				if (out == null) {
+					System.out.print("\nEnter an output file path:\n> ");
 					temp = get.readLine();
-					if (temp.equals("1"))
-					{
-						out.delete();
-						out.createNewFile();
-					}
-					else if (temp.equals("2"))
-					{
-						backupFile(out);
-						out.createNewFile();
-					}
-					else 
-					{
-						quitting = true;
+					// update our array
+					ioFilePaths[1] = new String(temp);
+					out = new File(temp);
+				} else {
+					// Checks if a file with the output file name already exists
+					if (out.exists()) {
+						System.out.print("\nAn output file with that name already exists."
+								+ "\nEnter 1 to overwrite it"
+								+ "\nEnter 2 to back it up, then write to the file"
+								+ "\nEnter 3 to enter a different name"
+								+ "\nEnter anything else to exit\n> ");
+						temp = get.readLine();
+						if (temp.equals("1")) {
+							out.delete();
+							out.createNewFile();
+							invalidOut = false;
+						}
+						else if (temp.equals("2")) {
+							backupFile(out);
+							out.createNewFile();
+							invalidOut = false;
+						}
+						else if (temp.equals("3")) {
+							out = null;
+						}
+						else {
+							quitting = true;
+						}
+					} else if (out.createNewFile()) {
+							invalidOut = false;	
 					}
 				}
-				
 			} catch (IOException e) {
-				e.printStackTrace();
+				e.printStackTrace();		
 			}
 		}
 		return !quitting;
 	}
 	
+	void print(PrintWriter out, int n, Word words[], int sum) {
+		for (int i = 0; i < n; i++) {
+			out.print("Word: " + words[i].getName());
+			out.print("\nCount: " + words[i].getCount() + "\n");
+		}
+		out.print("Total unique words: " + n);
+		out.print("\nSum: " + sum);
+	}
+	
 	// Changes the name of "theFile" to it's current name with "BACKUP" appended to the end
 	// Keeps the directory and extension of "theFile" intact
-	void backupFile(File theFile)
-	{
+	void backupFile(File theFile) {
 		File newFile = new File(getFileDirectory(theFile).concat(getFileNameNoExtension(theFile)).concat("BACKUP").concat(getFileExtension(theFile)));
 		// Checks if a file matching this backup name already exists
 		if (newFile.exists())
@@ -242,24 +245,22 @@ class IOFile {
 	}
 	
 	// Returns a String containing the extension of "theFile"
-	String getFileExtension(File theFile)
-	{
+	String getFileExtension(File theFile) {
 		return theFile.getName().substring(theFile.getName().indexOf('.'), theFile.getName().length());
 	}
 	
 	// Returns a String containing the name without the extension of "theFile"
-	String getFileNameNoExtension(File theFile)
-	{
+	String getFileNameNoExtension(File theFile) {
 		return theFile.getName().substring(0, theFile.getName().indexOf('.'));
 	}
 	
 	// Returns a String containing the directory of "theFile"
-	String getFileDirectory(File theFile)
-	{
+	String getFileDirectory(File theFile) {
 		return theFile.getPath().substring(0, theFile.getPath().length() - theFile.getName().length());
 	}
-
-	// Check if the string is just - or '
+	
+	
+    // Check if the string is just - or '
 	// We need this because we would get an ArrayIndexOutfBoundsException if we
 	// didn't have it
 	boolean good(String s) {
