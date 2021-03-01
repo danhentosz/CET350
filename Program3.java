2356981729fa14de8bd2fb5f853aa42986ca2d7b
Daniel Hentosz <57598807+danhentosz@users.noreply.github.com>
2021-03-01 14:29:48 -0500

    Add files via upload
    Changed .readLine() to .read() per the instructions. Added comments. Adding output for label 5(messages)

--- Program3.java
+++ Program3.java
@@ -21,6 +21,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 class Program3 extends Frame implements WindowListener, ActionListener {
+	// Creating our variables.
 	private static final long serialVersionUID = 1L;
 	Label l1 = new Label("Source:");
 	Label l2 = new Label();
@@ -36,7 +37,9 @@ class Program3 extends Frame implements WindowListener, ActionListener {
 	boolean source, target, outfile;
 	String targetname = "";
 
+	// Our main method
 	public static void main(String[] args) {
+		// Did they enter a command line parameter?
 		if (args.length == 1) {
 			try {
 				File f1 = new File(args[0]);
@@ -44,23 +47,33 @@ class Program3 extends Frame implements WindowListener, ActionListener {
 				if (f1.isDirectory()) {
 					new Program3(f1.getAbsolutePath());
 				} else {
+					// The command line parameter was not a valid directory path.
 					System.out.println("Enter a valid directory path.");
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		} else {
+			// If they did not enter any command line arguments.
+			// Getting the directory that the user is currently in.
 			new Program3(new File(System.getProperty("user.dir")).getAbsolutePath());
 		}
 	}
 
+	// This class creates a basic user interface using AWT.
+	// Frame, ActionListener, Buttons, Labels, TextFields, and the GridBagLAyout are
+	// all utilized.
 	Program3(String s) {
+		// Will be used to keep track of our current location.
 		curDir = new File(s);
+		// Used to keep track of which buttons and fields are enabled.
 		source = false;
 		target = false;
 		outfile = false;
+		// Our Layout manager.
 		GridBagLayout gbl = new GridBagLayout();
 		GridBagConstraints gbc = new GridBagConstraints();
+		// Each row and column has a width of 10.
 		double colweight[] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
 		double rowweight[] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
 		int width[] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
@@ -69,8 +82,12 @@ class Program3 extends Frame implements WindowListener, ActionListener {
 		gbl.columnWidths = width;
 		gbl.columnWeights = colweight;
 		gbl.rowWeights = rowweight;
+		// Making sure all the areas fit and do not overlap.
+		this.pack();
+		// Setting the window size.
 		this.setBounds(20, 20, 700, 350);
 		this.setLayout(gbl);
+		// Constraints for list.
 		gbc.anchor = GridBagConstraints.CENTER;
 		gbc.weightx = 10;
 		gbc.weighty = 6;
@@ -81,6 +98,7 @@ class Program3 extends Frame implements WindowListener, ActionListener {
 		gbc.gridy = 0;
 		gbl.setConstraints(list, gbc);
 		this.add(list);
+		// Constraints for label 1.
 		gbc.gridy = 6;
 		gbc.gridx = 0;
 		gbc.gridwidth = 1;
@@ -91,6 +109,7 @@ class Program3 extends Frame implements WindowListener, ActionListener {
 		gbc.fill = GridBagConstraints.BOTH;
 		gbl.setConstraints(l1, gbc);
 		this.add(l1);
+		// Constraints for label 2.
 		gbc.gridx = 1;
 		gbc.gridwidth = 8;
 		gbc.weightx = 8;
@@ -100,6 +119,7 @@ class Program3 extends Frame implements WindowListener, ActionListener {
 		gbc.anchor = GridBagConstraints.EAST;
 		gbl.setConstraints(l2, gbc);
 		this.add(l2);
+		// Constraints for button 1.
 		gbc.gridx = 0;
 		gbc.gridy = 7;
 		gbc.gridwidth = 1;
@@ -110,6 +130,7 @@ class Program3 extends Frame implements WindowListener, ActionListener {
 		gbc.anchor = GridBagConstraints.WEST;
 		gbl.setConstraints(b1, gbc);
 		this.add(b1);
+		// Constraints for label 3.
 		gbc.gridx = 1;
 		gbc.gridwidth = 8;
 		gbc.weightx = 8;
@@ -119,6 +140,7 @@ class Program3 extends Frame implements WindowListener, ActionListener {
 		gbc.fill = GridBagConstraints.BOTH;
 		gbc.anchor = GridBagConstraints.EAST;
 		this.add(l3);
+		// Constraints for label 4.
 		gbc.gridx = 0;
 		gbc.gridy = 8;
 		gbc.gridwidth = 1;
@@ -127,18 +149,21 @@ class Program3 extends Frame implements WindowListener, ActionListener {
 		gbc.weighty = 1;
 		gbl.setConstraints(l4, gbc);
 		this.add(l4);
+		// Constraints for our text field.
 		gbc.gridx = 1;
 		gbc.gridwidth = 8;
 		gbc.weightx = 8;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
 		gbl.setConstraints(txt, gbc);
 		this.add(txt);
+		// Constraints for label 5.
 		gbc.gridx = 0;
 		gbc.gridy = 9;
 		gbc.gridwidth = 9;
 		gbc.weightx = 9;
 		gbl.setConstraints(l5, gbc);
 		this.add(l5);
+		// Constraints for button 2.
 		gbc.gridx = 9;
 		gbc.gridy = 8;
 		gbc.gridwidth = 1;
@@ -147,36 +172,36 @@ class Program3 extends Frame implements WindowListener, ActionListener {
 		gbc.weighty = 1;
 		gbl.setConstraints(b2, gbc);
 		this.add(b2);
+		// Adding our action listeners to each necessary component.
 		b1.addActionListener(this);
 		b2.addActionListener(this);
 		list.addActionListener(this);
 		txt.addActionListener(this);
+		// Disabling our button and text field.
 		b1.setEnabled(false);
 		txt.setEnabled(false);
-		// Do we need to pack it? It says to do this in the lectures(class 8)
-		// this.pack();
 		this.setVisible(true);
 		this.addWindowListener(this);
-		// Call the display method
+		// Call the display method.
 		display(s);
 	}
 
 	void display(String s) {
 		list.removeAll();
-		// Making the title the current path, as long as its not the root
-
+		// Making the title the current path, as long as its not the root.
 		if (curDir.toPath().getNameCount() >= 2)
 			list.add("...");
 		else
 			l5.setText("You're at the root");
-		if (curDir.toPath().getNameCount() >= 1) {
+		if (curDir.toPath().getNameCount() >= 1)
 			this.setTitle(s);
-		}
+		// An array of all the children.
 		String names[] = curDir.list();
 		if (names != null) {
 			for (int i = 0; i < names.length; i++) {
 				File f2 = new File(curDir.getAbsolutePath() + "\\" + names[i]);
 				if (f2.isDirectory()) {
+					// Checking if the directory has a sub directory, so we can add "+".
 					String names2[] = f2.list();
 					boolean status = false;
 					if (names2 != null) {
@@ -189,26 +214,34 @@ class Program3 extends Frame implements WindowListener, ActionListener {
 						}
 					}
 				}
+				// Adding all the directory/file names
 				list.add(names[i]);
 			}
 		}
 	}
 
+	// When an action is performed(a button clicked or a text field and ENTER), it
+	// is sent here.
 	@Override
 	public void actionPerformed(ActionEvent ae) {
+		// Making an object of the action performed.
 		Object source = ae.getSource();
+		// Button 1(Target).
 		if (source == b1) {
 			txt.setEnabled(true);
 			target = true;
 			targetname = curDir.getAbsolutePath();
 			l3.setText(curDir.getAbsolutePath());
 		} else if (source == b2) {
+			// Button 2(OK).
 			copy();
 		} else if (source == list) {
+			// The user clicked an item in the list.
 			l5.setText("");
 			String name = list.getSelectedItem();
 			if (name.equals("...")) {
-				if (curDir.toPath().getNameCount() == 1)
+				// They want to go to the parent directory.
+				if (curDir.toPath().getNameCount() == 1)// They are at the root, do not continue.
 					l5.setText("You're at the root");
 				else {
 					int pos = 0;
@@ -217,6 +250,7 @@ class Program3 extends Frame implements WindowListener, ActionListener {
 					display(curDir.getAbsolutePath());
 				}
 			} else if (name.endsWith("+")) {
+				// If they want to open a directory with sub directories.
 				name = name.substring(0, name.length() - 1);
 				File temp = new File(curDir.getAbsolutePath() + "\\" + name);
 				if (temp.isDirectory()) {
@@ -224,6 +258,7 @@ class Program3 extends Frame implements WindowListener, ActionListener {
 					display(curDir.getAbsolutePath());
 				}
 			} else if (!name.contains(".")) {
+				// They clicked a file.
 				File temp = new File(curDir.getAbsolutePath() + "\\" + name);
 				if (temp.isDirectory()) {
 					curDir = new File(curDir.getAbsolutePath() + "\\" + name);
@@ -232,10 +267,11 @@ class Program3 extends Frame implements WindowListener, ActionListener {
 			} else {
 				l2.setText(curDir.getAbsolutePath() + "\\" + name);
 				b1.setEnabled(true);
-				if (txt.isEnabled())
+				if (txt.isEnabled())// Only set the text field when our target button is enabled.
 					txt.setText(name);
 			}
 		} else if (source == txt) {
+			// The user pressed enter when in the text field.
 			copy();
 			l2.setText("");
 			l3.setText("                                                                                  ");
@@ -244,21 +280,30 @@ class Program3 extends Frame implements WindowListener, ActionListener {
 		}
 	}
 
+	// Our copy method used to display the correct messages for label 5(Our messages
+	// label).
 	void copy() {
 		l5.setText("");
+		// Used to see if there is a duplicate file.
 		boolean dup = false;
+		// Used to see if the file was copied correctly.
 		boolean good = false;
+		// If all the information fields have data.
 		if (!l2.getText().isEmpty() && !l3.getText().isEmpty() && !l3.getText().contains("  ")
 				&& !txt.getText().isEmpty()) {
+			// Checking if label 2 is a valid file path.
 			File f1 = new File(l2.getText());
 			if (!f1.isFile())
 				l5.setText("Invalid source file");
+			// Checking is label 3 is a valid directory path.
 			File f2 = new File(l3.getText());
 			if (!f2.isDirectory())
 				l5.setText("Invalid target directory.");
+			// If both the conditions previously tested are true.
 			if (f1.isFile() && f2.isDirectory()) {
 				File f3 = new File(l3.getText());
 				String names[] = f3.list();
+				// Checking if the file already exists.
 				for (int i = 0; i < names.length; i++) {
 					if (names[i].equals(txt.getText())) {
 						dup = true;
@@ -268,12 +313,17 @@ class Program3 extends Frame implements WindowListener, ActionListener {
 				try {
 					BufferedReader get = new BufferedReader(new FileReader(l2.getText()));
 					PrintWriter out = new PrintWriter(new FileWriter(l3.getText() + "\\" + txt.getText()));
-					String line = "";
-					while ((line = get.readLine()) != null) {
-						out.println(line);
+					int line;
+					// Reading the file using integers, per the program Instructions.
+					// Typically we would use .readLine(), however, we were instructed to use
+					// .read().
+					while ((line = get.read()) != -1) {
+						out.write(line);
 					}
+					// Only change the text in label 5 if the file was not a duplicate.
 					if (!dup)
 						l5.setText("File Copied");
+					// Close our PrintWriter object.
 					out.close();
 					l2.setText("");
 					l3.setText(
@@ -287,9 +337,11 @@ class Program3 extends Frame implements WindowListener, ActionListener {
 				}
 			}
 		}
-		if (!good) {
+		if (!good) {// If copy was unsuccessful, we display the correct message in label 5.
 			if (l2.getText().isEmpty() && l3.getText().contains("  ") && txt.getText().isEmpty())
 				l5.setText("Source file, target directory, and target file not specified.");
+			else if (l3.getText().contains("  ") && txt.getText().isEmpty())
+				l5.setText("Target directory and target file not specified.");
 			else if (l2.getText().isEmpty() || l2.getText().equals(""))
 				l5.setText("Source file not specified.");
 			else if (l3.getText().contains("  "))
@@ -299,6 +351,19 @@ class Program3 extends Frame implements WindowListener, ActionListener {
 		}
 	}
 
+	// This method is used when the user wishes to close the window.
+	@Override
+	public void windowClosing(WindowEvent ae) {
+		// Removing our listeners so they are erased from memory.
+		this.removeWindowListener(this);
+		b1.removeActionListener(this);
+		b2.removeActionListener(this);
+		txt.removeActionListener(this);
+		this.dispose();
+	}
+
+	// We do not use the rest of the methods, however, they must be in our code.
+	// Due to us implementing WindowListener.
 	@Override
 	public void windowActivated(WindowEvent arg0) {
 		// TODO Auto-generated method stub
@@ -312,32 +377,18 @@ class Program3 extends Frame implements WindowListener, ActionListener {
 	}
 
 	@Override
-	public void windowClosing(WindowEvent ae) {
-		this.removeWindowListener(this);
-		this.dispose();
-	}
-
-	@Override
 	public void windowDeactivated(WindowEvent arg0) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void windowDeiconified(WindowEvent arg0) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void windowIconified(WindowEvent arg0) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void windowOpened(WindowEvent arg0) {
-		// TODO Auto-generated method stub
-
 	}
 }
